c7e53e56075feb386308ffcdd397689c4b16116c
ATHULKNAIR <77236185+ATHULKNAIR@users.noreply.github.com>
2021-05-07 12:46:59 +0530

    Recursion

--- Day-9-Recursions.js
+++ Day-9-Recursions.js
@@ -1,9 +1,11 @@
 
 function factorial(n){
-    var fact =1 ;
-   for(let i = n;i>0;i--){
+   
+    var fact =1 ;       
+   for(let i = n;i>0;i--){     // Get factorial of the number
        fact = fact * i
    }
    return fact;
+    
 }
 
